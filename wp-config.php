da9da2dab76d1417d87632abf902362e5ef689e3
Janagiramanm <janagiraman@netiapps.com>
2021-08-14 10:46:59 +0530

    debug

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'HijkLMNO$$1234$$' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '68.183.88.16' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
@@ -48,14 +48,24 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'd{C8i:x^H!_ilsl.NUkH1w^zBRG,{|lOvv6f#M(+;g?zKTbt=$9L`TO7JtOWE^Y]' );
-define( 'SECURE_AUTH_KEY',  'dnlq$xB}WqmF+QWUY2y-Xjh@7R?3#mJ5|f^Y2K`EAi<[(R0|J.vuCo1.X0*E9o]j' );
-define( 'LOGGED_IN_KEY',    'Zh%p9R.c$[2{t;[Cat>,cQBM<n|`N;69Y]<o0}Mm^M7LP9Y%=c51P)}WU:)Fh&;4' );
-define( 'NONCE_KEY',        'T 0i75k@meAkvwSo[6UGz4.]CWrYJ}1Dd45_ej%/ z%qUshyr KW*3B{ERqjOU;m' );
-define( 'AUTH_SALT',        '?st?aq*0i{`k06~yVq.Oh&MyQ](i-hqTk4+Z)Oqg.TS?t!y_:OfK9MeYOLAz;a=}' );
-define( 'SECURE_AUTH_SALT', 'ucD<BQK`yF}IE@3jY]X1,xS:Lz)WE5Ne#sDNaJ`t`Y)|u_n@5~5yWImius6k/^(y' );
-define( 'LOGGED_IN_SALT',   'NZrm7M3Vsuz-yb}FkCuL;PZX<2L#ljB#FH$a:0@N.US;l*q+tQ<M)rNiZ` Bi: ,' );
-define( 'NONCE_SALT',       ',ff,fSV_{S!.g/l7k=crq;;(h?]2tL=w-&^,.drr&kvfm$PB_p$^&zWR*]vU%D.u' );
+// define( 'AUTH_KEY',         'd{C8i:x^H!_ilsl.NUkH1w^zBRG,{|lOvv6f#M(+;g?zKTbt=$9L`TO7JtOWE^Y]' );
+// define( 'SECURE_AUTH_KEY',  'dnlq$xB}WqmF+QWUY2y-Xjh@7R?3#mJ5|f^Y2K`EAi<[(R0|J.vuCo1.X0*E9o]j' );
+// define( 'LOGGED_IN_KEY',    'Zh%p9R.c$[2{t;[Cat>,cQBM<n|`N;69Y]<o0}Mm^M7LP9Y%=c51P)}WU:)Fh&;4' );
+// define( 'NONCE_KEY',        'T 0i75k@meAkvwSo[6UGz4.]CWrYJ}1Dd45_ej%/ z%qUshyr KW*3B{ERqjOU;m' );
+// define( 'AUTH_SALT',        '?st?aq*0i{`k06~yVq.Oh&MyQ](i-hqTk4+Z)Oqg.TS?t!y_:OfK9MeYOLAz;a=}' );
+// define( 'SECURE_AUTH_SALT', 'ucD<BQK`yF}IE@3jY]X1,xS:Lz)WE5Ne#sDNaJ`t`Y)|u_n@5~5yWImius6k/^(y' );
+// define( 'LOGGED_IN_SALT',   'NZrm7M3Vsuz-yb}FkCuL;PZX<2L#ljB#FH$a:0@N.US;l*q+tQ<M)rNiZ` Bi: ,' );
+// define( 'NONCE_SALT',       ',ff,fSV_{S!.g/l7k=crq;;(h?]2tL=w-&^,.drr&kvfm$PB_p$^&zWR*]vU%D.u' );
+
+// this is for pantheon server
+define('AUTH_KEY',         'hqAmZw:5z7dRk-Y6_-TN]<&S+# Fp+v`#jl~6ujqkh[l094gVFO/ r8K-eUHy?cb');
+define('SECURE_AUTH_KEY',  'Yye<PsY5+W%-N7~|O=[6|iSkc54K &7In=/qK@o+R .-*Ms?fA{[ylf,9<OuAY._');
+define('LOGGED_IN_KEY',    'ca2sI5U/H%A+4Rk!I}2)7;Mr*LO?smA8hD+_piyfR_*nKg=}>k@wIV5*G}*Y%&Y9');
+define('NONCE_KEY',        '-y()T53VB+316~aoTCY4,2-(ktvACLw1oaN,_YK8_Ts+H(*wd{*!;h-8Gz#2]J{w');
+define('AUTH_SALT',        'v9kmP{P^QY`#+g`Kg7?X:YYIEc0BSQubf=OfACQ-hq_Wo/Bj+EL<Txzi33#.4Swq');
+define('SECURE_AUTH_SALT', '}1-Un$HR@ug|fxm$Oxbd(R)bUBG[cT~3[[W+xhd?fb/L,G`1t9ct;s nvxwe8Ss#');
+define('LOGGED_IN_SALT',   '&u)h*z|5<^.80T=oAk@R-w-$H>Hp-:UNtPGK@C<,|:KQ=IlH)*4s=^;mm59!y6`-');
+define('NONCE_SALT',       'GfS!&SR>zb|*u&i}iX,%(BvAg_b1L9WX9+?B3=KS-ioi/n-]mItM+g/dyp=p-&+:');
 
 /**#@-*/
 
