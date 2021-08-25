1221d8d67260028a54e5773e20d675752cafed18
Janagiramanm <janagiraman@netiapps.com>
2021-08-25 15:35:15 +0530

    debug

--- wp-content/plugins/buoyant-custom-post-types/index.php
+++ wp-content/plugins/buoyant-custom-post-types/index.php
@@ -8,3 +8,4 @@
  */
 
 require WP_PLUGIN_DIR."/buoyant-custom-post-types/buoyant-post-types/articles-and-stories.php";
+require WP_PLUGIN_DIR."/buoyant-custom-post-types/buoyant-post-types/information-post-types.php";
