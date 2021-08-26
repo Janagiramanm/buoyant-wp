799a8d7fa988df1d3d048e3d49547194a6232e34
Janagiramanm <janagiraman@netiapps.com>
2021-08-26 16:59:44 +0530

    debug

--- wp-content/plugins/custom-acf-rest-api/index.php
+++ wp-content/plugins/custom-acf-rest-api/index.php
@@ -205,7 +205,7 @@ function get_article($data){
                            while(have_rows('related_articles')):the_row();
                                 $relatedArticle =get_sub_field('article_name');
                                 $articleGroup = get_field_object('articles_stories',$relatedArticle->ID);
-                                var_dump($articleGroup);
+                                var_dump($articleGroup->title);
                                 // while():the_row();
                                 //       $res['related_articles'][] = [ 
                                 //            'banner_image' => get_sub_field('banner_image')
