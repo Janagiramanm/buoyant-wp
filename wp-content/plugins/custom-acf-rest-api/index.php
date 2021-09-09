3d9ebd7a8a8aa553233f3764c78b2a26261a30d9
Janagiramanm <janagiraman@netiapps.com>
2021-09-09 12:42:34 +0530

    debug

--- wp-content/plugins/custom-acf-rest-api/index.php
+++ wp-content/plugins/custom-acf-rest-api/index.php
@@ -298,7 +298,8 @@ function send_contact_us($request){
     // $request_body = file_get_contents('php://input');
     // $input = json_decode($request_body);
     echo '<pre>';
-    print_r($request);
+   print_r($request->get_json_params());
+   // print_r($request);
 
 //    $parameters = WP_REST_Request::parse_body_params();
 
