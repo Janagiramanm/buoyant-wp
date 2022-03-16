42efdf2938cf260a31656b61ca651a4b8385bcf6
Janagiramanm <janagiraman@netiapps.com>
2022-03-16 07:03:52 +0530

    db changes and moved to staging

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'buoyant_wp_db' );
+define( 'DB_NAME', 'buoyant_wp_db_staging' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
